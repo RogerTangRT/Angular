f6bf0f4c2ebaed9be2772cb6b0f82d46d87840bd
Roger Tang <rogertangrt@hotmail.com>
2022-05-12 11:36:28 -0300

    Criando Componente Nav-Bar

---
+++ netflix/src/app/shared/nav-bar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component(
+    {
+        selector:'app-nav-bar',
+        templateUrl:'./nav-bar.component.html',
+        styleUrls:['./nav-bar.component.scss']
+    }
+
+)
+export class NavBarComponent implements OnInit
+{
+    construtor()
+    {
+
+    }
+    ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
+   
+}
