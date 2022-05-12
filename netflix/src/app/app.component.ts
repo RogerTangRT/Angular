4fbe55ffe05e39c998e583261e5b12f974704ab7
Roger Tang <rogertangrt@hotmail.com>
2022-05-12 13:21:28 -0300

    Passagem de Parâmetros

--- netflix/src/app/app.component.ts
+++ netflix/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  listTitles = 
+  [
+    { treiler:'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+      cover:'assets/images/guardios-da-galaxia.jpg' ,
+      name:'Guardiões da Galaxia',
+      relevance:98,
+      age:0,
+      parts:2,
+      categories:['Épico','Filme de fantasia','Viagem no espaço']
+    },
+    { treiler:'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+      cover:'assets/images/guardios-da-galaxia.jpg' ,
+      name:'Guardiões da Galaxia 2',
+      relevance:80,
+      age:12,
+      parts:2,
+      categories:['Épico','Filme de fantasia','Viagem no espaço']
+    },
+    { treiler:'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+      cover:'assets/images/guardios-da-galaxia.jpg' ,
+      name:'Guardiões da Galaxia 3',
+      relevance:55,
+      age:14,
+      parts:2,
+      categories:['Épico','Filme de fantasia','Viagem no espaço']
+    },
+    { treiler:'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+      cover:'assets/images/guardios-da-galaxia.jpg' ,
+      name:'Guardiões da Galaxia 4',
+      relevance:55,
+      age:18,
+      parts:2,
+      categories:['Épico','Filme de fantasia','Viagem no espaço']
+    }
+  ];
 }
--- netflix/src/app/shared/title/title.component.ts
+++ netflix/src/app/shared/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TitleComponent implements OnInit {
 
+  @Input() title:any;
   constructor() { }
 
   ngOnInit(): void {
