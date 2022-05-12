cf82c069acb094c10b5732921948f9310e557a39
Roger Tang <rogertangrt@hotmail.com>
2022-05-12 17:14:47 -0300

    Mudança diretório Nav-bar

--- netflix/src/app/app.module.ts
+++ netflix/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './shared/nav-bar.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 import { FullBannerComponent } from './shared/full-banner/full-banner.component';
 import { ListTitlesComponent } from './shared/list-titles/list-titles.component';
 import { FooterComponent } from './shared/footer/footer.component';
