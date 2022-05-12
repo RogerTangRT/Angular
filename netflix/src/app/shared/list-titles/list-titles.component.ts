53901b586101ec7c335cbf2b663a7a88c603d4bb
Roger Tang <rogertangrt@hotmail.com>
2022-05-12 12:29:56 -0300

    Arrumando Title

--- netflix/src/app/shared/list-titles/list-titles.component.ts
+++ netflix/src/app/shared/list-titles/list-titles.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-list-titles',
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTitlesComponent implements OnInit {
 
-  constructor() { }
+  @Input() title: string | undefined;
+  constructor() {
+   }
 
   ngOnInit(): void {
   }
